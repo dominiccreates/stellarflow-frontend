f0e2970d1c60bcba53c2c64fd86c106799ac1a03
nanaabdul1172 <abdulrahmanfirdausionize@gmail.com>
2026-06-01 08:40:13 +0100

    Bundle Auditing

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzerConfig = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: false,
+  swcMinify: true,
+  compress: true,
+  productionBrowserSourceMaps: false,
+  optimizeFonts: true,
+  optimizePackageImports: [
+    "lucide-react",
+    "react-icons",
+  ],
 };
 
-export default nextConfig;
+export default withBundleAnalyzerConfig(nextConfig);
